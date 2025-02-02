f13b5e3c45c8c62dfa0fdb8c8f8d5c35574eba69
Abdul Haseeb Memon <ahm3m0n@gmail.com>
2025-02-02 05:53:13 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+
+
+import Blog from "@/components/blog";
+import Herro from "@/components/herro";
+import Priceplan from "@/components/plan";
+import Portfolio from "@/components/portfolio";
+import Recommendations from "@/components/recommendations";
+
+
+export default function Home() {
+  return (
+         <div>
+          <Herro/>
+          <Priceplan/>
+          <Recommendations/>
+          <Portfolio/>
+          <Blog/>
+         </div>
+  );
+}
---
+++ src/components/about.tsx
@@ -0,0 +1,17 @@
+import "../style/about.css";
+
+function Aboutus(){
+    return(
+        <div className="about-container">
+           <div className="about">
+            <h2>About Me</h2>
+           </div>
+           <div className="about-box2">
+                    <p className="about-title">I am <span className="bold">Abdul-Haseeb-memon</span>, a passionate and detail-oriented Web Developer and AI enthusiast. My journey spans across various domains, including Web Development, Data Analytics, and UI/UX Design, with hands-on experience in technologies like Next.js, Tailwind CSS, and TypeScript.</p>
+                    <p className="about-title">I am currently pursuing advanced learning at the <span className="bold">Governor IT Initiative</span> under the guidance of top mentors like <span className="bold">Sir Hamzah Syed</span> and <span className="bold">Sir Ameen Alam.</span> Alongside my technical expertise, I focus on freelancing and content creation, helping others grow through educational YouTube videos on web development and AI topics.</p>
+                    <p className="about-title">With a keen eye for design and a goal to leave a lasting impact, I recently created professional business cards and built responsive projects such as an e-commerce platform. I am always excited to collaborate and take on new challenges that align with my passion for technology and problem-solving.</p>
+                </div>
+        </div>
+    )
+}
+export default Aboutus
